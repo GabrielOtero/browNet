c5778e48170be3512154e541fcb1233cd9b60e60
Gabriel Otero <gabriel@gabriel-desktop.(none)>
2014-06-16 00:01:31 -0300

    Criando script de test

---
+++ Test/TestRunner.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+./node_modules/jasmine-node-karma/bin/jasmine-node-karma Test/jasmine-node
+
+jasmine_node_exit_code=$?
+
+
+./node_modules/bower/bin/bower install jasmine 
+
+./node_modules/phantomjs/bin/phantomjs ./Test/jasmine/phantomScript.js
+
+jasmine_front_exit_code=$?
+
+if [ $jasmine_node_exit_code -eq 0 ] && [ $jasmine_front_exit_code -eq 0 ]; then
+	echo "All test passed! :D"
+	exit 0
+else
+	echo "There are tests with errors! :("
+	exit 1
+fi
+
+
--- Test/jasmine/phantomScript.js
+++ Test/jasmine/phantomScript.js
@@ -7,11 +7,9 @@ page.open("file://"+dir+"/Test/jasmine/SpecRunner.html", function(status) {
 	    return document.body.getElementsByClassName("bar passed");
   	});
 
-  	if(passed.length === 1){
-  		console.info("Success")
-		phantom.exit(0);
+  if(passed.length === 1){
+		  phantom.exit(0);
 	}else{
-  		console.info("Fail")
   		phantom.exit(1);
   	}
 });
