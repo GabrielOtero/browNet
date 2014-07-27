4d14a5e5238ed4d0865162f55d01ae4dfccdbb78
Gabriel Otero <gabrielotero@MacBook-Pro-de-Gabriel.local>
2014-07-27 20:44:36 -0300

    extracting player

---
+++ App/frontend/js/player.js
@@ -0,0 +1,32 @@
+function Player() {
+	this.status = STATUS.PLAYING,
+	this.cards = [],
+	this.addCard = function (card) {
+		this.cards.push(card)
+	},
+	this.getScore = function () {
+		var score = 0;
+		
+		this.cards.sort(function(a, b){if(a == "A") {return 1} else{ return -1}});
+
+		this.cards.forEach(function (elem, idx, array) {
+			if(elem === "A"){
+				if(score <= 10){
+					score += 11
+				}else{
+					score += 1
+				}
+			}else if(elem === "K"){
+				score += 13;
+			}else if( elem === "Q"){
+				score += 12;
+			}else if( elem === "J"){
+				score += 11;
+			}else{
+				score += parseInt(elem);
+			}
+		});
+
+		return score;
+	}
+}
